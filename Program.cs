5686990d396f84483a42dc75f7cf289eb0d58670
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-27 22:35:29 +0530

    [Chandra]

--- Program.cs
+++ Program.cs
@@ -1,53 +1,16 @@
-﻿namespace DataStructures
+﻿namespace QueueUsingLinkedList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //  Console.WriteLine("Hello, World!");
-
-            //LinkedLists obj= new LinkedLists();
-            //obj.AddNode(20);
-            //obj.AddNode(30);
-            //obj.AddNode(40);
-            //obj.PrintList();
-
-            //var dataa = new LinkedList<string>();
-
-            // Exceptionss.Errors();
-
-            //  Regexx result = new Regexx();
-
-            //Console.WriteLine( result.Checkimage("image1.png"));
-            // Console.WriteLine(result.Checkwebsite("www.example.com"));
-            // Console.WriteLine(result.Checkmatch("abbb"));
-            // Console.WriteLine(result.Checkunderscore("a_b_c"));
-            //result.CheckHtmlTags("<p>The <code>Regex</code> is a compiled representation of a regular expression.</p>");
-
-
-            //Methods myDelegate = new Methods(Class1.Invokemethod);
-            //myDelegate("I used the delegate");
-
-            //Methods mydelegate = (message) => { Console.WriteLine(message); };
-            //mydelegate("Used Delegate with Lamda expression");
-
-            Console.WriteLine("----------LinkedList-----------");
-            LinkedListss.BuiltinLinkedlist();
-            Console.WriteLine("----------Stack-------------");
-            LinkedListss.BuiltinStack();
-            Console.WriteLine("---------------Queue---------------");
-            LinkedListss.BuiltinQueue();
-            Console.WriteLine("--------------SortedLinkedLisst-------------");
-            LinkedListss.BuiltinSortedList();
-            //
-            Console.WriteLine("-----------CUSTOM LINKEDLISTS-------------");
-            CustomLists add= new CustomLists();
-            add.AddNode(50);
-            add.AddNode(200);
-            add.Display();
-        
-
-
+            Console.WriteLine("Hello World!");
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();// this.top=this.top.next
+            Console.ReadKey();
         }
     }
 }
